b0a509d25e6eec9e6ee917d6e7b61f1d09f5a7fd
Ali Ganji <aliganji142879@gmail.com>
2024-06-30 19:08:39 +0330

    create view product list and product detail

--- products/models.py
+++ products/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.PositiveBigIntegerField(default=0)
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(default=True)  # active
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'pk': self.pk})
