ae30bdc866fbbfb3219be680d992222c4b47bc11
Ali Ganji <aliganji142879@gmail.com>
2024-07-03 14:15:12 +0330

    delete product in cart and __iter__ in cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,5 +1,11 @@
+from products.models import Product
+
+
 class Cart:
     def __init__(self, request, ):
+        """
+        Initialise the cart
+        """
         self.request = request
         self.session = request.session
 
@@ -11,6 +17,9 @@ class Cart:
         self.cart = cart
 
     def add(self, product, quantity=1):
+        """
+        Add a product to the cart
+        """
         product_id = str(product.id)
 
         if product_id not in self.cart:
@@ -20,5 +29,30 @@ class Cart:
 
         self.save()
 
+    def remove(self, product):
+        """
+        Remove a product from the cart
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
     def save(self):
+        """
+        Save the cart
+        """
         self.session.modified = True
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+
+        products = Product.objects.filter(id__id=product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product_obj'] = product
+
+        for item in cart.values():
+            yield item
