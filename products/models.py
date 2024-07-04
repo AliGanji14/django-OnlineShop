2f0d9d16d7c74c10987efcfd5435bc882c3360b2
Ali Ganji <aliganji142879@gmail.com>
2024-07-04 15:01:18 +0330

    install pillow and image product

--- products/models.py
+++ products/models.py
@@ -8,7 +8,8 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.PositiveBigIntegerField(default=0)
-    status = models.BooleanField(default=True)  # active
+    status = models.BooleanField(default=True)
+    image = models.ImageField(verbose_name=_('product image'), upload_to='product/product_cover/', blank=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
