b167c5427e0b78710d7b444586443e7ad2e32f5b
Ali Ganji <aliganji142879@gmail.com>
2024-07-08 19:24:28 +0330

    edit templates and add view account profile

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     path("comment/<int:pk>/", views.CommentCreateView.as_view(), name='comment_create'),
+    path('search/', views.search_product_view, name='product_search'),
+    path('profile/', views.profile_account_view, name='account_profile'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import get_object_or_404, reverse
+from django.shortcuts import get_object_or_404, render, HttpResponse
 from django.views import generic
 from django.contrib import messages
 from django.utils.translation import gettext as _
+from orders.models import Order
 
 from .forms import CommentForm
 from .models import Product, Comment
@@ -11,6 +12,7 @@ class ProductListView(generic.ListView):
     queryset = Product.objects.filter(status=True)
     template_name = 'products/product_list.html'
     context_object_name = 'products'
+    paginate_by = 2
 
 
 class ProductDetailView(generic.DetailView):
@@ -39,3 +41,17 @@ class CommentCreateView(generic.CreateView):
         messages.success(self.request, _('Comment successfully created'))
 
         return super().form_valid(form)
+
+
+def search_product_view(request):
+    if 'search' in request.GET:
+        search = request.GET['search']
+        products = Product.objects.filter(title__icontains=search)
+        if len(products) == 0:
+            return HttpResponse(_('Your product was not found'))
+    return render(request, 'products/product_list.html', {'products': products})
+
+
+def profile_account_view(request):
+    orders = Order.objects.filter(user=request.user.id)
+    return render(request, 'profile_account.html', {'orders': orders})
